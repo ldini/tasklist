121d3ac63a4a23a814af600b4bf11b03206eba13
Leandro <leandro.david.dini@gmail.com>
2023-11-07 05:47:08 -0300

    tasklist db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [
     TypeOrmModule.forRoot( {
       "type": "mysql",
-      "host": "localhost",
+      "host": "b2898xacxtxaxvhcmqt1-mysql.services.clever-cloud.com",
       "port": 3306,
-      "username": "root",
-      "password": "motorola",
-      "database": "prueba1",
+      "username": "udhdbcvo7uztcap2",
+      "password": "Dwwb1tSHILAurtoWDF81",
+      "database": "b2898xacxtxaxvhcmqt1",
       "entities": [
             "dist/**/**.entity{.ts,.js}"
       ],
